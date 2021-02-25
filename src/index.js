045cf37f29489a70d85fbf53f6f2c2038b28ff33
Apirat Chiwakanit <poom_popping@hotmail.com>
2021-02-25 22:52:00 +0700

    update test upload file s3

--- src/index.js
+++ src/index.js
@@ -1,43 +1,43 @@
-const express = require("express");
-const fs = require("fs");
-const AWS = require("aws-sdk");
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const AWS = require('aws-sdk');
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json({ limit: "10mb", extended: true }));
+app.use(express.json({ limit: '10mb', extended: true }));
 
-AWS.config.update({ region: "ap-southeast-1" });
+AWS.config.update({ region: 'ap-southeast-1' });
 
 const s3 = new AWS.S3({
-    accessKeyId: process.env.AWS_ID,
-    secretAccessKey: process.env.AWS_SECRET,
+	accessKeyId: process.env.ACCESS_KEY_ID,
+	secretAccessKey: process.env.SECRET_ACCESS_KEY,
 });
 
-app.use("/upload-file", (req, res) => {
+app.post('/upload-file', (req, res) => {
+	const fileContent = fs.readFileSync(path.resolve('src', 'mobile.jpg'));
+	const s3params = {
+		Bucket: process.env.PUBLIC_BUCKET_NAME,
+		Key: 'newfilename.jpg',
+		Body: fileContent,
+	};
 
-    const fileContent = fs.readFileSync(req.fileName);
-    const s3params = {
-        Bucket: "name",
-        Key: "filename.jpg",
-        Body: fileContent,
-    };
+	s3.upload(s3params, (err, data) => {
+		if (err) {
+			throw err;
+		}
+		console.log(`File uploaded successfully. ${data.Location}`);
+	});
 
-    s3.upload(s3params, function (err, data) {
-        if (err) {
-            throw err;
-        }
-        console.log(`File uploaded successfully. ${data.Location}`);
-    });
-
-    return res.json({
-        status: "ok",
-        message: "File uploaded successfully.",
-        data: data.Location,
-    });
+	return res.json({
+		status: 'ok',
+		message: 'File uploaded successfully.',
+		data: data.Location,
+	});
 });
 
 const port = process.env.PORT || 3001;
 
 app.listen(port || 3001, () => {
-    console.log(`Server is listening on port ${port}`);
+	console.log(`Server is listening on port ${port}`);
 });
