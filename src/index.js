0a94ee627dd04efd563dc36d2403d8666196f965
Apirat Chiwakanit <apirat.c@kbtg.tech>
2021-03-10 13:39:52 +0700

    update upload file to s3

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const fs = require('fs');
-const path = require('path');
 const AWS = require('aws-sdk');
+
+require("dotenv").config();
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
@@ -10,30 +11,29 @@ app.use(express.json({ limit: '10mb', extended: true }));
 AWS.config.update({ region: 'ap-southeast-1' });
 
 const s3 = new AWS.S3({
-	accessKeyId: process.env.ACCESS_KEY_ID,
-	secretAccessKey: process.env.SECRET_ACCESS_KEY,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 });
 
 app.post('/upload-file', (req, res) => {
-	const fileContent = fs.readFileSync(path.resolve('src', 'mobile.jpg'));
+	const fileContent = fs.readFileSync("./src/mobile.jpg");
 	const s3params = {
-		Bucket: process.env.PUBLIC_BUCKET_NAME,
-		Key: 'newfilename.jpg',
-		Body: fileContent,
-	};
+        Bucket: process.env.AWS_PUBLIC_BUCKET_NAME,
+        Key: `${Math.floor(Math.random() * 10)}-${new Date().getTime()}.jpg`,
+        Body: fileContent,
+    };
 
 	s3.upload(s3params, (err, data) => {
-		if (err) {
-			throw err;
-		}
-		console.log(`File uploaded successfully. ${data.Location}`);
-	});
+        if (err) {
+        	throw err;
+        }
 
-	return res.json({
-		status: 'ok',
-		message: 'File uploaded successfully.',
-		data: data.Location,
-	});
+		res.json({
+			status: "ok",
+			message: "File uploaded successfully.",
+			data: data.Location
+		});
+    });
 });
 
 const port = process.env.PORT || 3001;
